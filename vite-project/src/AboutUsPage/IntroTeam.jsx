4f1e374087f7b325e7602671cd5775aa46c04d2a
SeigiMondragon <sonic92yeylol@gmail.com>
2025-03-06 02:56:17 +0800

    back to css

---
+++ vite-project/src/AboutUsPage/IntroTeam.jsx
@@ -0,0 +1,26 @@
+import styles from "./styles/IntroTeam.module.css"
+
+function IntroTeam(){
+
+    return(
+       
+        <div className={styles.meetTheTeam}>
+            <div className={styles.leftCard}>
+                <div className={styles.upperLeft}>
+                  <h2>Lets Meet Our</h2>
+                  <h1>SDPT Academy Team</h1>
+                </div>
+                <div className={styles.lowerLeft}>   
+
+                </div>
+            
+            </div>
+            <div className={styles.rightCard}>
+
+            </div>
+        </div>
+    )
+
+}
+
+export default IntroTeam
