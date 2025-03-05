32520eb7ad10a043e12d3e8b5af7732b80f70fae
ver09 <oliver.baldomero09@gmail.com>
2025-03-05 23:24:15 +0800

    f

--- vite-project/src/NavBar.jsx
+++ vite-project/src/NavBar.jsx
@@ -1,9 +1,13 @@
+import Logo from './assets/sdpt logo.png'
+
 function NavBar () {
  return (
     <div className="navbar-container">
-       <nav>
-        <img src="/assets/sdpt logo.png" alt="" />
-       </nav>
+       
+        <img src= {Logo} alt=" pfp" />
+        <p>fsdfdas</p>
+        <p>fdasklfasdkdas</p>
+      
     </div>
 
  );
