a5255d3d7adc64bc6d29dc07beb4426142c585fd
SeigiMondragon <sonic92yeylol@gmail.com>
2025-03-05 23:09:39 +0800

    TryingRouter

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,10 +1,11 @@
 import Header from './Header'
-import { HashRouter,Router, Route } from 'react-router-dom'
+import { HashRouter,Routes, Route } from 'react-router-dom'
 function App() {
 
 
   return (
     <>
+
       <HashRouter>
         <Routes>
           <Route path='/' element={<Home/>} />
