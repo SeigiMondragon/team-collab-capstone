f77bfe9d243d80c3275f3817bf2a606b228d95f2
ver09 <oliver.baldomero09@gmail.com>
2025-03-05 23:35:03 +0800

    f

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,11 +1,12 @@
 import Header from './Header'
+import NavBar from './NavBar'
 import { HashRouter,Routes, Route } from 'react-router-dom'
 function App() {
 
 
   return (
     <>
-
+    <NavBar/>
    
     </>
   )
--- vite-project/src/NavBar.jsx
+++ vite-project/src/NavBar.jsx
@@ -1,13 +1,13 @@
 import Logo from './assets/sdpt logo.png'
+import { Link } from 'react-router-dom';
 
 function NavBar () {
  return (
     <div className="navbar-container">
-       
-        <img src= {Logo} alt=" pfp" />
-        <p>fsdfdas</p>
-        <p>fdasklfasdkdas</p>
-      
+        <img className='header-logo' src= {Logo} alt=" pfp" />
+        <Link to="/"> Home </Link>
+        <Link to="/AboutUs"> About Us </Link>
+        <Link to="/Courses"> Courses </Link>
     </div>
 
  );
