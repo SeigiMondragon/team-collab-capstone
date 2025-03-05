79e80efd234b8afb6afef3459058a070305deb9a
Jimsalm <hypermood15.jimiel@gmail.com>
2025-03-05 22:16:38 +0800

    YES YES NIGGA

--- vite-project/src/Header.jsx
+++ vite-project/src/Header.jsx
@@ -6,7 +6,8 @@ function Header(){
 
         <div>
             <h1>Hello World</h1>  
-            <h2>Hello Kalibutan</h2>  
+            <h2>Hello Kalibutan</h2>
+            <p>NOT NIGGA</p>  
         </div>
  
     );
