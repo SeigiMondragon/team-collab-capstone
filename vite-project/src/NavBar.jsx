d18b9bd52f5772e475434855402c62a7cd98a12f
ver09 <oliver.baldomero09@gmail.com>
2025-03-05 23:51:15 +0800

    f

--- vite-project/src/NavBar.jsx
+++ vite-project/src/NavBar.jsx
@@ -1,13 +1,21 @@
 import Logo from './assets/sdpt logo.png'
+import HeaderLogo from './assets/header bg.png'
 import { Link } from 'react-router-dom';
+
 
 function NavBar () {
  return (
-    <div className="navbar-container">
+
+    <div className="header-container">
+        
+        <div className='nav-container'>
         <img className='header-logo' src= {Logo} alt=" pfp" />
-        <Link to="/"> Home </Link>
-        <Link to="/AboutUs"> About Us </Link>
-        <Link to="/Courses"> Courses </Link>
+        <div className= 'navlinks-container' >
+        <Link className='navlink' to="/"> Home </Link>
+        <Link className='navlink' to="/AboutUs"> About Us </Link>
+        <Link className='navlink' to="/Courses"> Courses </Link>
+        </div>
+        </div>
     </div>
 
  );
