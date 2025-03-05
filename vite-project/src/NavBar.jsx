956d72f75106938f01b6a767805c8ffbacce248c
ver09 <oliver.baldomero09@gmail.com>
2025-03-05 23:10:09 +0800

    f

---
+++ vite-project/src/NavBar.jsx
@@ -0,0 +1,12 @@
+function NavBar () {
+ return (
+    <div className="navbar-container">
+       <nav>
+        <img src="/assets/sdpt logo.png" alt="" />
+       </nav>
+    </div>
+
+ );
+}
+
+export default NavBar
--- vite-project/src/NavBar/AboutUs.jsx
+++ vite-project/src/NavBar/AboutUs.jsx
@@ -2,7 +2,7 @@ function AboutUs(){
 
     return(
 
-        
+        <div></div>
     )
 
 }
