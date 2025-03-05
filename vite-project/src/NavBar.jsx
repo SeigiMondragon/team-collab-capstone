c947a06ac28b34ec85b574fb5e3cc8cb61a2df9c
ver09 <oliver.baldomero09@gmail.com>
2025-03-06 00:36:33 +0800

    f

--- vite-project/src/NavBar.jsx
+++ vite-project/src/NavBar.jsx
@@ -1,21 +1,33 @@
 import Logo from './assets/sdpt logo.png'
-import HeaderBG from './assets/header bg.png'
+import HomeIcon from './assets/Home Icon.png'
+import AboutIcon from './assets/About Icon.png'
+import CoursesIcon from './assets/Courses Icon.png'
+import ProfileIcon from './assets/Profile Icon.png'
 import { Link } from 'react-router-dom';
 
 
 function NavBar () {
  return (
 
-    <div className="header">    
-        <div className='nav-container'>
-        <img className='header-logo' src= {Logo} alt=" pfp" />
-        <div className= 'navlinks-container' >
-        <Link className='navlink' to="/"> Home </Link>
-        <Link className='navlink' to="/AboutUs"> About Us </Link>
-        <Link className='navlink' to="/Courses"> Courses </Link>
-        </div>
-        </div>
+    <div className="header">
+    <div className="nav-container">
+      <img className="header-logo" src={Logo} alt="pfp" />
+      <div className="navlinks-container">
+        <Link className="navlink" to="/">
+          <img className="nav-icon" src={HomeIcon} alt="Home" />
+        </Link>
+        <Link className="navlink" to="/AboutUs">
+          <img className="nav-icon" src={AboutIcon} alt="About Us" />
+        </Link>
+        <Link className="navlink" to="/Courses">
+          <img className="nav-icon" src={CoursesIcon} alt="Courses" />
+        </Link>
+        <Link className="navlink" to="/Courses">
+          <img className="nav-icon-profile" src={ProfileIcon} alt="Courses" />
+        </Link>
+      </div>
     </div>
+  </div>
 
  );
 }
