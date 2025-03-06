01ed1a3489857e613954629611c1869ac2294293
SeigiMondragon <sonic92yeylol@gmail.com>
2025-03-06 20:07:08 +0800

    clean

--- vite-project/src/Header.jsx
+++ vite-project/src/Header.jsx
@@ -5,13 +5,7 @@ function Header(){
     return(
 
         <div>
-            <h1>Hello World</h1>  
-            <h2>Hello Kalibutan</h2>
-            <p>NOT NIGGA</p>  
-            <p>No, Im a nigga</p>
-            <p>flkadshfsadlkfasf</p>
-            <p>hello</p>
-            <p>Hello po</p>
+
         </div>
  
     );
