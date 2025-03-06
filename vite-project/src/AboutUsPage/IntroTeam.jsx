8ba5c86f2713cb73c3cdde39e50f69cb152e6ffc
SeigiMondragon <sonic92yeylol@gmail.com>
2025-03-06 20:00:02 +0800

    pa pull

--- vite-project/src/AboutUsPage/IntroTeam.jsx
+++ vite-project/src/AboutUsPage/IntroTeam.jsx
@@ -12,13 +12,13 @@ function IntroTeam(){
                 </div>
                 <div className={styles.lowerLeft}>   
                     <div>Developer|Designer|Professor|Marketing</div>
-                     <div>2 members </div>
+                    <div className={styles.members}>2 members </div>
                 </div>
             
             </div>
             <div className={styles.rightCard}>
                 <hr className={styles.horizontal} />
-        
+            
             </div>
         </div>
     )
